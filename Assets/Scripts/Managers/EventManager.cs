482be2558eead95a28e54d631a82040316a19297
Wouter <wouterbomer@gmail.com>
2020-08-06 16:39:58 +0200

    Events: - Added a custom event messaging system to keep instances seperated, thus making every part testable on it's own without being coupled to something else. - Custom events can be added to components and can be selected from that component if so desired, this should make triggering events from custom locations easier.

---
+++ Assets/Scripts/Managers/EventManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventManager : MonoBehaviour
+{
+    public enum CustomEventType
+    {
+        EVENT_NONE = 0,
+        EVENT_PLAYER_START_GAME = 1,
+    }
+
+    // Singleton
+    private static EventManager instance;
+
+    public static EventManager Instance
+    {
+        get { return instance; }
+    }
+
+    // Collections
+    private Dictionary<CustomEventType, UnityEvent> eventLibrary;
+
+    // Awake is called at initialization of this class
+    void Awake()
+    {
+        // Singleton creation
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<EventManager>();
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+
+        eventLibrary = new Dictionary<CustomEventType, UnityEvent>();
+    }
+
+    public void AddListener(CustomEventType eventType, UnityAction listener)
+    {
+        // Check if already existing
+        UnityEvent unityEvent = null;
+        bool exists = eventLibrary.TryGetValue(eventType, out unityEvent);
+
+        if (exists)
+        {
+            // Already exists, add listener to event
+            unityEvent.AddListener(listener);
+        } 
+        else
+        {
+            // Create a new one
+            unityEvent = new UnityEvent();
+            unityEvent.AddListener(listener);
+            eventLibrary.Add(eventType, unityEvent);
+        }
+    }
+
+    public void RemoveListener(CustomEventType eventType, UnityAction listener)
+    {
+        // Check if already existing
+        UnityEvent unityEvent = null;
+        bool exists = eventLibrary.TryGetValue(eventType, out unityEvent);
+
+        // Remove listener
+        if (exists)
+            unityEvent.RemoveListener(listener);
+    }
+
+    public void RemoveAllListeners()
+    {
+        foreach (KeyValuePair<CustomEventType, UnityEvent> kvp in eventLibrary)
+        {
+            kvp.Value.RemoveAllListeners();
+        }
+    }
+
+    public void RemoveAllListenersFor(CustomEventType eventType)
+    {
+        // Check if already existing
+        UnityEvent unityEvent = null;
+        bool exists = eventLibrary.TryGetValue(eventType, out unityEvent);
+
+        unityEvent.RemoveAllListeners();
+    }
+
+    public void TriggerEvent(CustomEventType eventType)
+    {
+        // Check if already existing
+        UnityEvent unityEvent = null;
+        bool exists = eventLibrary.TryGetValue(eventType, out unityEvent);
+
+        // Fire event.
+        if (exists)
+            unityEvent.Invoke();
+    }
+}
