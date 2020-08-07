5e47f0511221d7fc8f41916ba3874056db758132
Wouter <wouterbomer@gmail.com>
2020-08-07 10:25:49 +0200

    User Interfaces: - Added UI manager for managing user interface life cycle

---
+++ Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Singleton
+    private static UIManager instance;
+
+    // Editorvariables
+    [Header("Canvas objects")]
+    public GameObject startMenuCanvas = null;
+    public GameObject optionsMenuCanvas = null;
+    public GameObject hudCanvas = null;
+
+    [Header("Initialization")]
+    public GameObject firstActiveCanvas = null;
+
+    // References
+    private GameObject currentActiveCanvas = null;
+
+    public static UIManager Instance
+    {
+        get { return instance; }
+    }
+
+    public void Awake()
+    {
+        // Singleton creation
+        if (instance == null)
+        {
+            instance = GameObject.FindObjectOfType<UIManager>();
+        }
+        else
+        {
+            Destroy(this);
+            return;
+        }
+
+        // Subscribe to custom events
+        EventManager.Instance.AddListener(EventManager.CustomEventType.EVENT_ACTIVATE_CANVAS, OnActivateCanvas);
+
+        // Set initial canvas active
+        ActivateCanvas(firstActiveCanvas);
+    }
+
+    private void OnActivateCanvas(System.Object args)
+    {
+        if (args is GameObject)
+            ActivateCanvas((GameObject)args);
+    }
+
+    private void ActivateCanvas(GameObject canvas)
+    {
+        if (canvas == null)
+        {
+#if UNITY_EDITOR
+            throw new System.Exception("Canvas is null");
+#else
+            return;
+#endif
+        }
+
+        // First deactivate the current canvas
+        DeActivateCanvas(currentActiveCanvas);
+
+        // Activate new one
+        canvas.SetActive(true);
+        currentActiveCanvas = canvas;
+    }
+
+    private void DeActivateCanvas(GameObject canvas)
+    {
+        if (canvas != null)
+            canvas.SetActive(false);
+    }
+}
