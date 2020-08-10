6b7f3629b08396b7e2c7be9fb57e43cafdac8609
Wouter <wouterbomer@gmail.com>
2020-08-10 12:33:06 +0200

    Camera: - Added a camera controller script which will make sure that the background of the game is always sized to the resoluction of the device that it is played on

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    // Editor variables
+    [Header("Camera variables")]
+    [Tooltip("The background object that will always be fit to screen orientation and size")]
+    public GameObject background;
+
+    // Objects
+    private DeviceOrientation currentOrientation;
+
+    void Awake()
+    {
+        if (background == null)
+            Debug.Log("CameraController.Awake(): No background gameobject assigned!");
+        else
+            AdjustBackgroundSize();  // Set initial background size and position
+
+#if UNITY_ANDROID
+        currentOrientation = Input.deviceOrientation;
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+#if UNITY_ANDROID
+        DetectOrientationChange();
+#endif
+    }
+
+    // TODO: Should be called when changing aspect from/to landscape/portrait, must be an event somewhere.
+    private void AdjustBackgroundSize()
+    {
+        if (background == null)
+            return;
+
+        // Get components
+        SpriteRenderer backgroundSpriteRenderer = background.GetComponent<SpriteRenderer>();
+
+        // Setup variables
+        float cameraHeight = Camera.main.orthographicSize * 2;
+        Vector2 cameraSize = new Vector2(Camera.main.aspect * cameraHeight, cameraHeight);
+        Vector2 spriteSize = backgroundSpriteRenderer.sprite.bounds.size;
+        Vector2 backgroundScale = Vector2.one;
+
+        // Determine landscape/portrait and adjust
+        backgroundScale.x = cameraSize.x / spriteSize.x;
+        backgroundScale.y = cameraSize.y / spriteSize.y;
+
+        // Set to background
+        background.transform.localScale = backgroundScale;
+    }
+
+    private void DetectOrientationChange()
+    {
+        if (currentOrientation != Input.deviceOrientation)
+            AdjustBackgroundSize();
+    }
+}
