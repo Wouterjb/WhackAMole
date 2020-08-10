55f5203e08a0779a6d3968cb617fda99acfb8563
Wouter <wouterbomer@gmail.com>
2020-08-10 15:28:09 +0200

    Fields: - Added a field controller to manage all the moles that need be whacked. - Added a hole object to determine which kind of mole is going to be spawned.

---
+++ Assets/Scripts/Actors/Hole.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class Hole : MonoBehaviour
+{
+    // Editor variabels
+    // TODO prefabs
+
+    // References
+    private MoleController activeMole = null;
+
+    public bool HasActiveMole
+    {
+        get { return activeMole != null; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void Activate()
+    {
+        // This can be done in a million ways; could just spawn one type of mole, randomly select a mole or keep track of types of moles and spawn depending on that etc..
+
+        // TODO;
+        // Spawn random mole
+    }
+}
---
+++ Assets/Scripts/Controllers/FieldController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+
+public class FieldController : MonoBehaviour
+{
+    // Editor variables
+    [Header("Field properties")]
+    [Tooltip("The maximum amount of moles visible")]
+    public int maxAmountOfMoles = 3; // This could also be dependant on a difficulty grade
+    [Tooltip("The minimum amount of time to wait for a mole to spawn")]
+    public int minSpawnInterval = 1;
+    [Tooltip("The maximum amount of time to wait for a mole to spawn")]
+    public int maxSpawnInterval = 3;
+
+    // Collections
+    private Hole[] holes = null;
+
+    // Numbers
+    private int activeAmountOfMoles = 0;
+    private float currentSpawnInterval = 0.0f;
+    private float currentSpawnTimer = 0.0f;
+
+    private int AmountOfHoles
+    {
+        get { return holes.Length; }
+    }
+
+    // Awake is called at initialization of this class
+    void Awake()
+    {
+        // Gather all holes that belong to this field
+        holes = this.gameObject.GetComponentsInChildren<Hole>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Limit max amount of moles by the amount of holes available.
+        maxAmountOfMoles = Mathf.Min(AmountOfHoles, maxAmountOfMoles);
+
+        // Generate new spawn time interval
+        GenerateNewSpawnInterval();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTimers();
+    }
+
+    private void UpdateTimers()
+    {
+        if (activeAmountOfMoles <= maxAmountOfMoles)
+        {
+            currentSpawnTimer += Time.deltaTime;
+
+            if (currentSpawnTimer >= currentSpawnInterval)
+            {
+                // Spawn new mole.
+                SpawnNewMole();
+
+                // Keep track of variables
+                activeAmountOfMoles++;
+                currentSpawnTimer = 0.0f;
+
+                // Generate new interval
+                GenerateNewSpawnInterval();
+            }
+        }
+    }
+
+    private void GenerateNewSpawnInterval()
+    {
+        currentSpawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+    }
+
+    private void SpawnNewMole()
+    {
+        // TODO
+        // Determine where to spawn a mole; when a hole already has a mole, do not spawn it there, but another place etc.
+    }
+}
